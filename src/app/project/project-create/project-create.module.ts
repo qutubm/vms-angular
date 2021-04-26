4fe0d252cbf693fb1670fe8443d18d2a72bdde1f
Lance Bongon <lbbongon@gmail.com>
2021-04-26 20:50:37 +1000

    The thing with an error

--- src/app/project/project-create/project-create.module.ts
+++ src/app/project/project-create/project-create.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { ProjectCreateRoutingModule } from './project-create-routing.module';
 import { ProjectCreateComponent } from './project-create.component';
+
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ProjectCreateComponent],
   imports: [
-    CommonModule,
     ProjectCreateRoutingModule,
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    CommonModule,
     FormsModule
   ]
 })
